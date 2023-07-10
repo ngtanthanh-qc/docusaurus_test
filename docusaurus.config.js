8b90fca6d203ef5479110073e55fa292a8df6ed8
Thanh Nguyen <ngtanthanh.qc@gmail.com>
2023-07-10 22:07:54 +0700

    Update docsearch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,85 +1,92 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Docs-n-Notes',
-  tagline: 'Knowledge is power. Sharing knowledge is the key to unlocking that power.',
-  favicon: 'img/favicon.ico',
-
+  title: "Docs-n-Notes",
+  tagline:
+    "Knowledge is power. Sharing knowledge is the key to unlocking that power.",
+  favicon: "img/favicon.ico",
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-live-codeblock", "@docusaurus/theme-mermaid"],
   // Set the production url of your site here
-  url: 'https://docs.tanthanh.dev',
+  url: "https://docs.tanthanh.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ngtanthanh-qc', // Usually your GitHub org/user name.
-  projectName: 'sharing docs and tips', // Usually your repo name.
+  organizationName: "ngtanthanh-qc", // Usually your GitHub org/user name.
+  projectName: "sharing docs and tips", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
-
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          sidebarPath: require.resolve("./sidebars.js"),
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
-
+  stylesheets: [
+    {
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
+      integrity:
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
+    },
+  ],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'Home',
+        title: "Home",
         logo: {
-          alt: 'Site Logo',
-          src: 'img/logo.svg',
+          alt: "Site Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Tutorial",
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: "/blog", label: "Blog", position: "left" },
           {
-            type: 'search',
-            position: 'right',
+            type: "search",
+            position: "right",
           },
           // {
           //   href: 'https://github.com/facebook/docusaurus',
@@ -88,14 +95,14 @@ const config = {
           // },
           // {
           //   type: 'docsVersionDropdown',
-          // },        
+          // },
           // {
           //   type: 'localeDropdown',
           // },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         // links: [
         //   {
         //     title: 'Docs',
@@ -143,7 +150,18 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      algolia: {
+        appId: "ISLH6D7EJC",
+        apiKey: "1699acc7aa3b0e45d2ff0b8a715157cb",
+        indexName: "tanthanh",
+        contextualSearch: true,
+        searchParameters: {},
+      },
+      mermaid: {
+        options: {
+          maxTextSize: 500,
+        },
+      },
     }),
 };
-
 module.exports = config;
